68bb30fb5e7bb713d8eede05228599ae5ade6785
davidwu-shaw <david_wu_lf@hotmail.com>
2020-06-25 16:47:25 -0400

    add ManageCustomerRoles permission and modify database to limit vendor's access to roles

--- src/Libraries/Nop.Services/Security/StandardPermissionProvider.cs
+++ src/Libraries/Nop.Services/Security/StandardPermissionProvider.cs
@@ -19,6 +19,7 @@ namespace Nop.Services.Security
         public static readonly PermissionRecord ManageProductTags = new PermissionRecord { Name = "Admin area. Manage Product Tags", SystemName = "ManageProductTags", Category = "Catalog" };
         public static readonly PermissionRecord ManageAttributes = new PermissionRecord { Name = "Admin area. Manage Attributes", SystemName = "ManageAttributes", Category = "Catalog" };
         public static readonly PermissionRecord ManageCustomers = new PermissionRecord { Name = "Admin area. Manage Customers", SystemName = "ManageCustomers", Category = "Customers" };
+        public static readonly PermissionRecord ManageCustomerRoles = new PermissionRecord { Name = "Admin area. Manage Customer Roles", SystemName = "ManageCustomerRoles", Category = "Customers" };
         public static readonly PermissionRecord ManageVendors = new PermissionRecord { Name = "Admin area. Manage Vendors", SystemName = "ManageVendors", Category = "Customers" };
         public static readonly PermissionRecord ManageCurrentCarts = new PermissionRecord { Name = "Admin area. Manage Current Carts", SystemName = "ManageCurrentCarts", Category = "Orders" };
         public static readonly PermissionRecord ManageOrders = new PermissionRecord { Name = "Admin area. Manage Orders", SystemName = "ManageOrders", Category = "Orders" };
@@ -80,6 +81,7 @@ namespace Nop.Services.Security
                 ManageProductTags,
                 ManageAttributes,
                 ManageCustomers,
+                ManageCustomerRoles,
                 ManageVendors,
                 ManageCurrentCarts,
                 ManageOrders,
@@ -146,6 +148,7 @@ namespace Nop.Services.Security
                         ManageProductTags,
                         ManageAttributes,
                         ManageCustomers,
+                        ManageCustomerRoles,
                         ManageVendors,
                         ManageCurrentCarts,
                         ManageOrders,
