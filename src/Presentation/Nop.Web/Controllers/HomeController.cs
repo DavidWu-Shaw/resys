ff372649dc9983b55d93145596dbebeda227b775
davidwu-shaw <david_wu_lf@hotmail.com>
2020-07-06 14:37:14 -0400

    redirect guest user to login page

--- src/Presentation/Nop.Web/Controllers/HomeController.cs
+++ src/Presentation/Nop.Web/Controllers/HomeController.cs
@@ -1,14 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Core;
 using Nop.Web.Framework.Mvc.Filters;
 using Nop.Web.Framework.Security;
+using Nop.Core.Domain.Customers;
 
 namespace Nop.Web.Controllers
 {
     public partial class HomeController : BasePublicController
     {
+        private readonly IWorkContext _workContext;
+
+        public HomeController(IWorkContext workContext)
+        {
+            _workContext = workContext;
+        }
+
         [HttpsRequirement(SslRequirement.No)]
         public virtual IActionResult Index()
         {
+            if (_workContext.CurrentCustomer.IsGuest())
+            {
+                return RedirectToRoute("Login");
+            }
             return View();
         }
     }
